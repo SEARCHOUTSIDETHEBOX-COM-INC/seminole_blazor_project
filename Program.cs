7dd0126868217f02dcc18b69ccd1338db1761242
Jeremy Wood <webmaster@searchoutsidethebox.com>
2023-06-22 08:59:17 -0400

    updated on visual code mac

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Seminole;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<AppDomain>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
